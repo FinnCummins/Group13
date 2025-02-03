dc1ceb530d8a760576b02ab2e50514308475d96f
Finn Cummins <finnkcummins@gmail.com>
2025-02-03 14:17:55 +0000

    Added containerised PostgreSQL Database with User table

---
+++ database/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    student_id VARCHAR(20) UNIQUE NOT NULL,
+    interests TEXT[],
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
