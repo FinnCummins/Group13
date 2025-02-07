15bb3167d3bcc707d7521282303a35fd453b4c50
markdennis282 <dennisma@tcd.ie>
2025-02-07 12:32:56 +0000

    added background to landing page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -17,13 +17,26 @@ export default function Home() {
 
       {/* Main*/}
       <main className="mt-20 text-center px-4">
-        <section className="bg-[var(--foreground)] text-[var(--background)] py-16">
+        <section className="bg-[var(--foreground)] text-[var(--background)] pt-16 pb-8">
           <h1 className="text-6xl font-bold mb-4">Welcome to Final Year Project Finder</h1>
-          <p className="text-lg mb-8">
+          <p className="text-lg">
             Find, research, and select the perfect final year project that aligns with your interests.
             Our built-in chatbot helps you discover projects.
           </p>
         </section>
+
+        <section
+  className="w-full h-[400px] bg-cover bg-center"
+  style={{
+    backgroundImage: "url('/trinity_background.jpg')", // Correct path to the image
+  }}
+>
+  <div className="h-full flex items-center justify-center">
+    <h2 className="text-4xl font-bold text-white">
+      Find the project that suits you best !
+    </h2>
+  </div>
+</section>
 
 
       </main>
