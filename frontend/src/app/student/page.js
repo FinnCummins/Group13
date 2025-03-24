c69082b5980e9301c6a24fca88d7f1ee592e04d0
Finn Cummins <finnkcummins@gmail.com>
2025-03-24 15:29:22 +0000

    Revert "removed redundant comment"
    This reverts commit 353d4950ec9dfe7286ef3379c1fc6a3997b43fb2.

--- frontend/src/app/student/page.js
+++ frontend/src/app/student/page.js
@@ -37,6 +37,10 @@ export default function StudentHomePage() {
     }
   }, [isClient]);
 
+  if (!isClient) {
+    return <div>Loading...</div>; // Or any loading state you prefer
+  }
+
   return (
     <div>
       <Head>
