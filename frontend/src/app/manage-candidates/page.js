280203098f9feec4b652db7a5cdbbe7bc63fc43f
markdennis282 <dennisma@tcd.ie>
2025-04-03 16:40:21 +0100

    changed get URL

--- frontend/src/app/manage-candidates/page.js
+++ frontend/src/app/manage-candidates/page.js
@@ -29,7 +29,8 @@ export default function SupervisorRequestsPage() {
         const apiUrl =
           process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:5001";
         const response = await fetch(
-          `${apiUrl}/api/requests?supervisor_id=${supervisorId}`
+          //`${apiUrl}/api/requests?supervisor_id=${supervisorId}`
+          `${apiUrl}/api/supervisor_requests/${supervisorId}`
         );
 
         if (!response.ok) {
