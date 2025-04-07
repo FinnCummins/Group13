f2ce27e1728cfa8583afc67289f4cd79293cb629
Dishant Tekwani <73080020+dishantekwanii@users.noreply.github.com>
2025-04-07 10:58:01 +0100

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,9 +1,1 @@
-class Config:
-    # General Config
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///:memory:')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-class TestConfig(Config):
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # Using in-memory SQLite for tests
